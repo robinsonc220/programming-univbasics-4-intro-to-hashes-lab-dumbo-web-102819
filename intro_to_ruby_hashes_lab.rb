e436a08d33ac9fe20ebeadc0fd3dfbc99dee2c72
Conrad Robinson <robinsonc220@gmail.com>
2019-10-21 19:38:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ end
 
 def update_counting_hash(hash, key)
   hash = {key => 4}
-  hash[key] = hash[key] + 1
+  hash[key] += 1
 end
 
   # given a hash an a key as parameters, return an updated hash
